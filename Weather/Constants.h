0295041412fb0a5a5d7d40154e6f099394f6149d
Josh Wright <joshuawright11@gmail.com>
2016-03-09 15:03:50 -0800

    General UI Finished.

---
+++ Weather/Constants.h
@@ -0,0 +1,20 @@
+//
+//  Constants.h
+//  Weather
+//
+//  Created by Josh Wright on 3/9/16.
+//  Copyright © 2016 WeDo. All rights reserved.
+//
+
+//** Colors
+
+// Main
+#define wColorMain [UIColor colorWithRed:(0x43)/255.0 green:(0xBD)/255.0 blue:(0xFF)/255.0 alpha:1]
+#define wColorTextMain [UIColor colorWithRed:(0x52)/255.0 green:(0x52)/255.0 blue:(0x52)/255.0 alpha:1]
+#define wColorTextLight [UIColor colorWithRed:(0xFF)/255.0 green:(0xFF)/255.0 blue:(0xFF)/255.0 alpha:1]
+
+// Sun Gradient Colors
+#define wColorSunriseTop [UIColor colorWithRed:(0xFA)/255.0 green:(0xD9)/255.0 blue:(0x61)/255.0 alpha:1]
+#define wColorSunriseBottom [UIColor colorWithRed:(0xF7)/255.0 green:(0x6B)/255.0 blue:(0x1C)/255.0 alpha:1]
+#define wColorSunsetTop [UIColor colorWithRed:(0x30)/255.0 green:(0x23)/255.0 blue:(0xAE)/255.0 alpha:1]
+#define wColorSunsetBottom [UIColor colorWithRed:(0x35)/255.0 green:(0x76)/255.0 blue:(0xF7)/255.0 alpha:1]
--- Weather/ViewController.m
+++ Weather/ViewController.m
@@ -7,8 +7,27 @@
 //
 
 #import "ViewController.h"
+#import "Constants.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *zipTextField;
+
+@property (weak, nonatomic) IBOutlet UILabel *todayLabel;
+@property (weak, nonatomic) IBOutlet UIView *dataBackgroundView;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *dataTitleLabels;
+
+@property (weak, nonatomic) IBOutlet UILabel *temperatureDataLabel;
+@property (weak, nonatomic) IBOutlet UILabel *humidityDataLabel;
+@property (weak, nonatomic) IBOutlet UILabel *windDataLabel;
+@property (weak, nonatomic) IBOutlet UILabel *cloudsDataLabel;
+
+@property (weak, nonatomic) IBOutlet UIView *sunBackgroundView;
+@property (strong, nonatomic) IBOutletCollection(UILabel) NSArray *sunDataTitleLabels;
+@property (weak, nonatomic) IBOutlet UILabel *sunriseDataLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sunsetDataLabel;
+
+@property (weak, nonatomic) IBOutlet UITableView *recentsTableView;
 
 @end
 
@@ -16,12 +35,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self doDesign];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)doDesign {
+    
+    // Background Colors
+    self.view.backgroundColor = wColorMain;
+    self.zipTextField.backgroundColor = [UIColor whiteColor];
+    
+    // Text Colors
+    self.todayLabel.textColor = [UIColor whiteColor];
+    self.sunriseDataLabel.textColor = [UIColor whiteColor];
+    self.sunsetDataLabel.textColor = [UIColor whiteColor];
+
+    for (UILabel* label in self.sunDataTitleLabels) {
+        label.textColor = [UIColor whiteColor];
+    }
+    
+    // Rounded Edges
+    self.dataBackgroundView.layer.cornerRadius = 9.0;
+    self.sunBackgroundView.layer.cornerRadius = 9.0;
+    self.recentsTableView.layer.cornerRadius = 9.0;
+    self.zipTextField.layer.cornerRadius = 9.0;
+}
+
+- (void)viewDidLayoutSubviews {
+    [self setupSunGradients];
+}
+
+- (void)setupSunGradients {
+    self.sunBackgroundView.clipsToBounds = YES;
+    [self addGradientToView:self.sunBackgroundView withTopColor:wColorSunriseTop bottomColor:wColorSunriseBottom leftHalf:YES];
+    [self addGradientToView:self.sunBackgroundView withTopColor:wColorSunsetTop bottomColor:wColorSunsetBottom leftHalf:NO];
+}
+
+- (void)addGradientToView:(UIView *)view withTopColor:(UIColor *)topColor bottomColor:(UIColor *)bottomColor leftHalf:(BOOL)left{
+    
+    // Create the gradient
+    CAGradientLayer *viewGradient = [CAGradientLayer layer];
+    viewGradient.colors = [NSArray arrayWithObjects: (id)topColor.CGColor, (id)bottomColor.CGColor, nil];
+    
+    CGFloat gradientFrameX = left ? 0 : view.bounds.size.width/2.0;
+    CGRect frame = CGRectMake(gradientFrameX, 0, view.bounds.size.width/2, view.bounds.size.height);
+    viewGradient.frame = frame;
+    
+    //Add gradient to view
+    [view.layer insertSublayer:viewGradient atIndex:0];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    return cell;
 }
 
 @end
