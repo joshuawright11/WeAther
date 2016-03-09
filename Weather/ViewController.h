99d8f8052e88cb3509be73906a5b1c5f6e35bf20
Josh Wright <joshuawright11@gmail.com>
2016-03-09 13:33:57 -0800

    Initial Commit

---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Josh Wright on 3/9/16.
+//  Copyright © 2016 WeDo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Weather/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Weather
+//
+//  Created by Josh Wright on 3/9/16.
+//  Copyright © 2016 WeDo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
